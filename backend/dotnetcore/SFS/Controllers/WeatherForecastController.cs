2bcbc3a81ded667cb48269677f8072b5598bf6bc
tarcisio <tarcisio.marinho>
2020-07-12 23:44:33 -0300

    adding dotnet core as backend

---
+++ backend/dotnetcore/SFS/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFS.Services;
+
+namespace SFS.Controllers
+{
+    //[ApiController]
+    [Route("api/[controller]")]
+    public class FileController : ControllerBase
+    {
+        private readonly FileService _fileService;
+
+        public FileController()
+        {
+            _fileService = new FileService();
+        }
+
+        // download file(s) to client according path: rootDirectory/subDirectory with single zip file
+        [HttpGet("Download/{subDirectory}")]
+        public IActionResult DownloadFiles(string subDirectory)
+        {
+            try
+            {
+                var (fileType, archiveData, archiveName) = _fileService.FetechFiles(subDirectory);
+
+                return File(archiveData, fileType, archiveName);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest($"Error: {exception.Message}");
+            }
+        }
+
+        // upload file(s) to server that palce under path: rootDirectory/subDirectory
+        [HttpPost("upload")]
+        public IActionResult UploadFile([FromForm(Name = "files")] List<IFormFile> files, string subDirectory)
+        {
+            try
+            {
+                _fileService.SaveFile(files, subDirectory);
+
+                return Ok(new { files.Count, Size = FileService.SizeConverter(files.Sum(f => f.Length)) });
+            }
+            catch (Exception exception)
+            {
+                return BadRequest($"Error: {exception.Message}");
+            }
+        }
+
+    }
+}
